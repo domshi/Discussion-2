7505e4f025dedc5ddfafb072a25664612982d744
domshi <90562421+domshi@users.noreply.github.com>
2025-01-23 18:11:46 +0000

    initial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,21 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
-
+import java.util.*;
+/*
+Dominic shriver
+Java 2805
+D2 assignment
+23 Jan 2025
+-main calss for the Scanner example code
+I DID NOT USE AI TO MAKE THIS PROGRAM
+*/
 public class Main {
+  
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    Scanner name = new Scanner(System.in);//creates the specific scanner for getting a name input
+    System.out.println("What is your name?");
+    String nameIn = name.nextLine();//retrives the name from the user 
+    System.out.println(nameIn);//prints the name retrived from user
+    
   }
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
 }
